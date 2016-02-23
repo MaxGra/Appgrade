7972b398d9652dfccb3f4a76ea58fd82f8d1979b
FabianZientek <fzientek@gmail.com>
2016-02-23 11:01:23 +0100

    Competence - Subject

--- client/teacherkurse.js
+++ client/teacherkurse.js
@@ -1,11 +1,11 @@
 Template.teacherkurse.helpers({
-    dataset: function(){
-      return teacherhassubject.select('subject.yearsdesc', 'subject.subjectdesc')
-      .join(['OUTER JOIN'], ["subjectsubjectid"], [["subject", ['subjectid']]])
- //     .join(['OUTER JOIN'], ['yearsyearsid'], [['years', ['yearsid']]])
-      .where('userid = ceic34fLMYPp7bFit')
-      .fetch();
-    }
+//    dataset: function(){
+//      return teacherhassubject.select('subject.yearsdesc', 'subject.subjectdesc')
+//      .join(['OUTER JOIN'], ["subjectsubjectid"], [["subject", ['subjectid']]])
+// //     .join(['OUTER JOIN'], ['yearsyearsid'], [['years', ['yearsid']]])
+//      .where('userid = ceic34fLMYPp7bFit')
+//      .fetch();
+//    }
     
 //    dataset: function(){
 //       return teacherhassubject.select('subjectdesc').fetch();
@@ -19,9 +19,9 @@ Template.teacherkurse.helpers({
 //    teacherhassubject: function () {
 //      return teacherhassubject.select().fetch();
 //    }
-//    competence: function () {
-//      return competence.select().fetch();
-//    },
+    competence: function () {
+      return competence.select().where('subjectid = 4').fetch();
+    },
 //    descriptor: function () {
 //      return descriptor.select().fetch();
 //    }
